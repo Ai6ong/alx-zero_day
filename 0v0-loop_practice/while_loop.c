51fcf1532216127c8af521a1524d6d9ddd6d8969
ai6ong <infoofficialisong@gmail.com>
2023-06-30 20:18:33 -0700

    updateed

---
+++ 0v0-loop_practice/while_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 1 to 5
+ *
+ * Return 0 (successful)
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 5)
+	{
+		printf("%d\n", i);
+		++i;
+	}
+	return (0);
+}
